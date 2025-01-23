f9299bf1a1dd945e5fb118e9ada5702f7a26d9a2
josephbouassaf <joseph.bouassaf@mail.mcgill.ca>
2025-01-23 16:58:35 -0500

    fixed link

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import { transaction } from "./transaction";
                 console.log('Error:', error); 
             });
         } else if(input === '3') {
-            console.log('\x1b[32m' + 'CONFIGURED STRATEGY: FIJA ETH VAULT (visit: https://fija.finance/)' + '\x1b[0m');
+            console.log('\x1b[32m' + 'CONFIGURED STRATEGY: FIJA ETH VAULT (visit: https://fija.finance/strategies)' + '\x1b[0m');
             const input = await askForInput('Do you wish to: \n [1] deposit into the strategy \n [2] withdraw from the strategy \n [3] exit \n Please choose one of the options listed above: '); 
 
             if(input === '1') {
