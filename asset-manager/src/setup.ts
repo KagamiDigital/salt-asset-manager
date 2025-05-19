918a17fcfaef4cdacc0ddce43dce884c2c91e6da
Caleb Yates <calebyates42@gmail.com>
2025-05-19 15:16:37 +1000

    add: updated all uses of transaction

--- asset-manager/src/setup.ts
+++ asset-manager/src/setup.ts
@@ -1,95 +1,90 @@
 import {
-  automateRegistration,
-  getUserPreRegisterInfos,
-  getUserRegistrationAllInfos,
-  preRegistration,
-  registerAllSteps,
-} from "@intuweb3/exp-node";
+	automateRegistration,
+	getUserPreRegisterInfos,
+	getUserRegistrationAllInfos,
+	preRegistration,
+	registerAllSteps,
+	getUserIndex,
+} from "intu";
 import { ethers } from "ethers";
 import { askForInput } from "./helpers";
 import { orchestration_network_provider, signer } from "./constants";
-import { getUserIndex } from "@intuweb3/exp-node/lib/services/web3/utils";
 
 const preRegisterBot = async (
-  accountAddress: string,
-  bot: ethers.Signer,
-  provider: ethers.providers.JsonRpcProvider,
+	accountAddress: string,
+	bot: ethers.Signer,
+	provider: ethers.providers.JsonRpcProvider,
 ) => {
-  const botAddress = await bot.getAddress();
-  try {
-    const preRegisterInfo = await getUserPreRegisterInfos(
-      accountAddress,
-      botAddress,
-      provider,
-    );
+	const botAddress = await bot.getAddress();
+	try {
+		const preRegisterInfo = await getUserPreRegisterInfos(
+			accountAddress,
+			botAddress,
+			provider,
+		);
 
-    if (preRegisterInfo.registered) {
-      console.log(
-        "The asset manager bot has already been pre-registered to this account.",
-      );
-      return;
-    }
-    console.log("pregegister:start");
-    const tx = (await preRegistration(
-      accountAddress,
-      bot,
-    )) as ethers.ContractTransaction;
-    const res = await tx.wait();
-    console.log("preregister:success");
-    return res;
-  } catch (err) {
-    console.log("pregister:failure", err);
-    return;
-  }
+		if (preRegisterInfo.registered) {
+			console.log(
+				"The asset manager bot has already been pre-registered to this account.",
+			);
+			return;
+		}
+		console.log("pregegister:start");
+		const tx = (await preRegistration(
+			accountAddress,
+			bot,
+		)) as ethers.ContractTransaction;
+		const res = await tx.wait();
+		console.log("preregister:success");
+		return res;
+	} catch (err) {
+		console.log("pregister:failure", err);
+		return;
+	}
 };
 
 const registerBot = async (
-  accountAddress: string,
-  bot: ethers.Signer,
-  provider: ethers.providers.JsonRpcProvider,
+	accountAddress: string,
+	bot: ethers.Signer,
+	provider: ethers.providers.JsonRpcProvider,
 ) => {
-  const botAddress = await bot.getAddress();
-  try {
-    const botIdx = await getUserIndex(accountAddress, botAddress, provider);
-    const registerAllInfo = await getUserRegistrationAllInfos(
-      accountAddress,
-      provider,
-    );
+	const botAddress = await bot.getAddress();
+	try {
+		const botIdx = await getUserIndex(accountAddress, botAddress, provider);
+		const registerAllInfo = await getUserRegistrationAllInfos(
+			accountAddress,
+			provider,
+		);
 
-    console.log("the bot idx is " + botIdx);
-    console.log(registerAllInfo);
+		console.log("the bot idx is " + botIdx);
+		console.log(registerAllInfo);
 
-    if (
-      registerAllInfo[botIdx] !== null &&
-      registerAllInfo[botIdx].registered
-    ) {
-      console.log("The asset manager bot has already been registered");
-      return;
-    }
-    console.log("automaticRegistration:start");
-    // open the nostr db connection
-    await automateRegistration(
-      accountAddress,
-      bot,
-      // https://docs.intu.xyz/intu-sdk-functions/core-functionality/automateregistration/#parameters
-      "wss://relay.nostrdice.com" as any,
-    );
+		if (
+			registerAllInfo[botIdx] !== null &&
+			registerAllInfo[botIdx].registered
+		) {
+			console.log("The asset manager bot has already been registered");
+			return;
+		}
+		console.log("automaticRegistration:start");
+		// open the nostr db connection
+		await automateRegistration(accountAddress, bot);
 
-    console.log("automaticRegistration:success");
+		console.log("automaticRegistration:success");
 
-    console.log("registerAllSteps:start");
+		console.log("registerAllSteps:start");
 
-    const tx = (await registerAllSteps(
-      accountAddress,
-      bot,
-    )) as ethers.ContractTransaction;
+		const tx = (await registerAllSteps(
+			accountAddress,
+			bot,
+		)) as ethers.ContractTransaction;
 
-    await tx.wait();
+		await tx.wait();
 
-    console.log("registerAllSteps:success");
-  } catch (err) {
-    console.log("register:failure", err);
-  }
+		console.log("registerAllSteps:success");
+	} catch (err) {
+		console.log("register:failure", err);
+	}
 };
 
 /**
@@ -101,41 +96,41 @@ const registerBot = async (
  *      in the account creation process successfully.
  */
 export async function setup() {
-  const vaultAddress = await askForInput(
-    `\nPlease enter the VAULT address from which you want to add the asset manager bot to: `,
-  );
+	const vaultAddress = await askForInput(
+		`\nPlease enter the VAULT address from which you want to add the asset manager bot to: `,
+	);
 
-  let done = false;
+	let done = false;
 
-  while (!done) {
-    if (!ethers.utils.isAddress(vaultAddress)) {
-      console.log("You need a valid account address to proceed.");
-    } else {
-      done = true;
-    }
-  }
+	while (!done) {
+		if (!ethers.utils.isAddress(vaultAddress)) {
+			console.log("You need a valid account address to proceed.");
+		} else {
+			done = true;
+		}
+	}
 
-  done = false;
+	done = false;
 
-  while (!done) {
-    const input = await askForInput(
-      "Do you wish to: \n [1] pre-register the bot \n [2] register the bot \n [3] exit \n Please choose one of the options listed above:  ",
-    );
-    if (input === "1") {
-      await preRegisterBot(
-        vaultAddress,
-        signer,
-        orchestration_network_provider,
-      );
-    } else if (input === "2") {
-      await registerBot(vaultAddress, signer, orchestration_network_provider);
-    } else if (input === "3") {
-      done = true;
-    } else {
-      console.log("Please enter a valid choice");
-      console.log(
-        "Do you wish to: \n [1] pre-register the bot \n [2] register the bot \n [3] exit \n Please choose one of the options listed above: \n",
-      );
-    }
-  }
+	while (!done) {
+		const input = await askForInput(
+			"Do you wish to: \n [1] pre-register the bot \n [2] register the bot \n [3] exit \n Please choose one of the options listed above:  ",
+		);
+		if (input === "1") {
+			await preRegisterBot(
+				vaultAddress,
+				signer,
+				orchestration_network_provider,
+			);
+		} else if (input === "2") {
+			await registerBot(vaultAddress, signer, orchestration_network_provider);
+		} else if (input === "3") {
+			done = true;
+		} else {
+			console.log("Please enter a valid choice");
+			console.log(
+				"Do you wish to: \n [1] pre-register the bot \n [2] register the bot \n [3] exit \n Please choose one of the options listed above: \n",
+			);
+		}
+	}
 }
--- asset-manager/src/transaction.ts
+++ asset-manager/src/transaction.ts
@@ -1,143 +1,148 @@
 import { BigNumber, ContractTransaction, ethers } from "ethers";
 import { askForInput } from "./helpers";
-import {
-  getVaultsWithoutTransactions,
-  signTx,
-  submitTransaction,
-} from "intu";
+import { getVaultsWithoutTransactions, signTx, submitTransaction } from "intu";
 import { broadcasting_network_provider, signer } from "./constants";
 import env from "./env";
 
 /** Will ask using stdin for missing arguments if not provided */
 export async function transaction(
-  vaultAddressI?: string | undefined,
-  recipientAddressI?: string | undefined,
-  amountI?: string | Number | undefined,
-  skipConfirmation?: boolean | undefined,
+	vaultAddressI?: string | undefined,
+	recipientAddressI?: string | undefined,
+	amountI?: string | Number | undefined,
+	skipConfirmation?: boolean | undefined,
 ) {
-  const vaultAddress =
-    vaultAddressI ||
-    (await askForInput(
-      `Please enter the account address from where you wish to execute the transfer: `,
-    ));
-  if (!ethers.utils.isAddress(vaultAddress)) {
-    throw "You need a valid account address to propose a transaction";
-  }
-
-  const managedVaults = await getVaultsWithoutTransactions(
-    signer.address,
-    signer.provider,
-  );
-
-  const recipientAddress =
-    recipientAddressI ||
-    (await askForInput(`Please enter the recipient's address: `));
-  if (!ethers.utils.isAddress(recipientAddress)) {
-    throw "You need a valid recipient address to propose a transaction";
-  }
-
-  const amount =
-    amountI || (await askForInput(`Please enter the amount to transfer: `));
-
-  // skips if string
-  if (+amount < 0) {
-    throw "You need a positive amount to propose a transaction";
-  }
-
-  console.info("Beginning transaction ...");
-  console.info("vaultAddress:", vaultAddress);
-  console.info("recipientAddress:", recipientAddress);
-  console.info("amount:", amount);
-
-  const vault = managedVaults.find(
-    (v) => v.masterPublicAddress === vaultAddress,
-  );
-
-  const nonce = await broadcasting_network_provider.getTransactionCount(
-    vault.masterPublicAddress,
-  );
-
-  // get the fee data on the broadcasting network
-  const feeData = await broadcasting_network_provider.getFeeData();
-
-	console.info("Using custom sending provider", env.BROADCASTING_NETWORK_RPC_NODE_URL, "chainID", env.BROADCASTING_NETWORK_ID);
-  // const sendingProvider = new ethers.providers.JsonRpcProvider(
-  //   env.BROADCASTING_NETWORK_RPC_NODE_URL,
-  //   // env.BROADCASTING_NETWORK_ID,
-  // );
-	const sendingProvider = broadcasting_network_provider;
-	const actualChainId = await sendingProvider.getNetwork().then((network) => network.chainId);
-	if (actualChainId !== Number(env.BROADCASTING_NETWORK_ID)) {
-		throw new Error(`Expected chain ID ${env.BROADCASTING_NETWORK_ID} but got ${actualChainId}`);
+	const vaultAddress =
+		vaultAddressI ||
+		(await askForInput(
+			`Please enter the account address from where you wish to execute the transfer: `,
+		));
+	if (!ethers.utils.isAddress(vaultAddress)) {
+		throw "You need a valid account address to propose a transaction";
 	}
-  const submitTransactionTx = await submitTransaction(
-    recipientAddress,
-    amount,
-    env.BROADCASTING_NETWORK_ID,
-    nonce,
-    "",
-    BigNumber.from(feeData.gasPrice).toNumber(),
-    21000,
-    vault.vaultAddress,
-    signer,
-    "SERVER",
-    false,
-    // this is technically undocumented, but otherwise
-    // this function relies on hard-coded RPC node URLs
-    // based on the network ID you provide (above),
-    // and somnia shannon isn't in this list which requires use to manually specify this
-    // sendingProvider as any,
-    broadcasting_network_provider as any
-  );
+
+	const managedVaults = await getVaultsWithoutTransactions(
+		signer.address,
+		signer.provider,
+	);
+
+	const recipientAddress =
+		recipientAddressI ||
+		(await askForInput(`Please enter the recipient's address: `));
+	if (!ethers.utils.isAddress(recipientAddress)) {
+		throw "You need a valid recipient address to propose a transaction";
+	}
+
+	const amount =
+		amountI || (await askForInput(`Please enter the amount to transfer: `));
+
+	// skips if string
+	if (+amount < 0) {
+		throw "You need a positive amount to propose a transaction";
+	}
+
+	console.info("Beginning transaction ...");
+	console.info("vaultAddress:", vaultAddress);
+	console.info("recipientAddress:", recipientAddress);
+	console.info("amount:", amount);
+
+	const vault = managedVaults.find(
+		(v) => v.masterPublicAddress === vaultAddress,
+	);
+
+	const nonce = await broadcasting_network_provider.getTransactionCount(
+		vault.masterPublicAddress,
+	);
+
+	// get the fee data on the broadcasting network
+	const feeData = await broadcasting_network_provider.getFeeData();
+
+	console.info(
+		"Using custom sending provider",
+		env.BROADCASTING_NETWORK_RPC_NODE_URL,
+		"chainID",
+		env.BROADCASTING_NETWORK_ID,
+	);
+	// const sendingProvider = new ethers.providers.JsonRpcProvider(
+	//   env.BROADCASTING_NETWORK_RPC_NODE_URL,
+	//   // env.BROADCASTING_NETWORK_ID,
+	// );
+	const sendingProvider = broadcasting_network_provider;
+	const actualChainId = await sendingProvider
+		.getNetwork()
+		.then((network) => network.chainId);
+	if (actualChainId !== Number(env.BROADCASTING_NETWORK_ID)) {
+		throw new Error(
+			`Expected chain ID ${env.BROADCASTING_NETWORK_ID} but got ${actualChainId}`,
+		);
+	}
+	const submitTransactionTx = await submitTransaction(
+		recipientAddress,
+		amount,
+		env.BROADCASTING_NETWORK_ID,
+		nonce,
+		"",
+		BigNumber.from(feeData.gasPrice).toNumber(),
+		21000,
+		vault.vaultAddress,
+		signer,
+		"SERVER",
+		false,
+		// this is technically undocumented, but otherwise
+		// this function relies on hard-coded RPC node URLs
+		// based on the network ID you provide (above),
+		// and somnia shannon isn't in this list which requires use to manually specify this
+		// sendingProvider as any,
+		broadcasting_network_provider,
+	);
 	console.info("submitTransaction", submitTransaction);
 
-  const submitTransactionResult = await (
-    submitTransactionTx as ContractTransaction
-  ).wait();
+	const submitTransactionResult = await (
+		submitTransactionTx as ContractTransaction
+	).wait();
 	console.info("submitTransactionResult", submitTransactionResult);
-  const submitTransactionEvents = submitTransactionResult.events;
-  const event = submitTransactionEvents[0];
+	const submitTransactionEvents = submitTransactionResult.events;
+	const event = submitTransactionEvents[0];
 
-  const eventData = {
-    txId: event.args[0]._hex,
-    txInfo: event.args[1],
-  };
+	const eventData = {
+		txId: event.args[0]._hex,
+		txInfo: event.args[1],
+	};
 
-  console.log("transaction submitted successfully", eventData.txId);
+	console.log("transaction submitted successfully", eventData.txId);
 
-  console.log("Note the transaction details:");
-  console.log("recipient: " + recipientAddress);
-  console.log("amount: " + amount);
-  console.log("chainId: " + env.BROADCASTING_NETWORK_ID);
-  console.log("nonce: " + nonce);
-  console.log(
-    "gasPrice: " +
-      ethers.utils
-        .parseEther(BigNumber.from(feeData.gasPrice).toString())
-        .toString(),
-  );
-  console.log("gas: 21000");
+	console.log("Note the transaction details:");
+	console.log("recipient: " + recipientAddress);
+	console.log("amount: " + amount);
+	console.log("chainId: " + env.BROADCASTING_NETWORK_ID);
+	console.log("nonce: " + nonce);
+	console.log(
+		"gasPrice: " +
+			ethers.utils
+				.parseEther(BigNumber.from(feeData.gasPrice).toString())
+				.toString(),
+	);
+	console.log("gas: 21000");
 
-  if (!skipConfirmation) {
-    const approval = await askForInput(
-      `\nPlease confirm you want to sign the transaction, you cannot cancel the transaction after this point? [yes/no] `,
-    );
+	if (!skipConfirmation) {
+		const approval = await askForInput(
+			`\nPlease confirm you want to sign the transaction, you cannot cancel the transaction after this point? [yes/no] `,
+		);
 
-    if (approval !== "yes") {
-      console.log("You haven't manually approved the transaction");
-      console.log("transaction proposal cannot carry on without the signature");
-      return;
-    }
-  }
+		if (approval !== "yes") {
+			console.log("You haven't manually approved the transaction");
+			console.log("transaction proposal cannot carry on without the signature");
+			return;
+		}
+	}
 
-  const tx = (await signTx(
-    vault.vaultAddress,
-    Number(eventData.txId),
-    signer,
-  )) as ethers.ContractTransaction;
+	const tx = (await signTx(
+		vault.vaultAddress,
+		Number(eventData.txId),
+		signer,
+	)) as ethers.ContractTransaction;
 
 	console.info("Waiting for tx ...");
-  await tx.wait();
+	await tx.wait();
 
-  console.log("Transaction signed successfully");
+	console.log("Transaction signed successfully");
 }
