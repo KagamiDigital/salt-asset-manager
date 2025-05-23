2a47ccf72750ab995435c5c0e20fe56eca985987
Caleb Yates <calebyates42@gmail.com>
2025-05-23 14:56:33 +1000

    clean: removed all aave strategy stuff for the moment

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
 import { askForInput, printRectangle, rl } from "./helpers";
-// import { approve, deposit, withdraw } from "./strategy/aave/aave";
-async function approve() {
-	throw new Error("TODO");
-}
-async function deposit() {
-	throw new Error("TODO");
-}
-async function withdraw() {
-	throw new Error("TODO");
-}
 import { transaction } from "./transaction";
 import { handleCLIArgs } from "./cli";
 import { Env } from "./env";
@@ -31,7 +21,7 @@ if (await handleCLIArgs(config)) {
 let done = false;
 
 const question =
-	"Do you wish to: \n [1] make a native currency transfer \n [2] execute a strategy \n [3] exit \n Please choose one of the options listed above: ";
+	"Do you wish to: \n [1] make a native currency transfer \n [2] ~~execute a strategy~~ (removed) \n [3] exit \n Please choose one of the options listed above: ";
 
 while (!done) {
 	const input = await askForInput(question);
@@ -40,28 +30,7 @@ while (!done) {
 			console.error("Error:", error);
 		});
 	} else if (input === "2") {
-		const strategy_q =
-			"Do you wish to: \n [1] deposit into the strategy \n [2] approve funds withdrawal from the strategy \n [3] withdraw from the strategy \n [4] exit \n Please choose one of the options listed above: ";
-		const input = await askForInput(strategy_q);
-
-		if (input === "1") {
-			await deposit().catch((error) => {
-				console.error("Error:", error);
-			});
-		} else if (input === "2") {
-			await approve().catch((error) => {
-				console.log("Error:", error);
-			});
-		} else if (input === "3") {
-			await withdraw().catch((error) => {
-				console.log("Error:", error);
-			});
-		} else if (input === "4") {
-			done = true;
-		} else {
-			console.log("Please enter a valid choice");
-			console.log(strategy_q);
-		}
+		console.error("removed");
 	} else if (input === "3") {
 		done = true;
 	} else {
